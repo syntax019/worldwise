cfabbb732c5cd57504593e35e9920276498a4af2
syntax019 <edubiosamuelvictor@gmail.com>
2024-02-29 03:11:35 +0100

    editing AppNav

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,6 @@
+import styles from "./AppNav.module.css";
+function AppNav() {
+  return <div className={styles.nav}>App navigation</div>;
+}
+
+export default AppNav;
---
+++ src/pages/Product.jsx
@@ -0,0 +1,5 @@
+function Product() {
+  return <div>Products</div>;
+}
+
+export default Product;
